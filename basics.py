15c6fbd9612f8e7543548c084f4d6f6fc9a4918d
Peter <pi.dudka@gmail.com>
2020-04-27 06:52:09 -0400

    Playing around with more data types

--- basics.py
+++ basics.py
@@ -1,3 +1,26 @@
 import datetime
 mynow = datetime.datetime.now()
 print ("The date and time is:", mynow)
+
+
+""" Data Types """
+
+# Integers vs. floats
+x = 10
+y = 10.1
+z = "10"
+
+# Lists
+student_grades = [8.3, 8.8, 7.5]
+
+# You can use dir() to get info about a class or method
+# You can use help() to get info about how to use a class or method
+
+mysum = sum(student_grades) / len(student_grades)
+print(mysum)
+
+# Dictionaries
+more_student_grades = {
+    "Peter": 10.5,
+    "Sarah": 9.5
+}
