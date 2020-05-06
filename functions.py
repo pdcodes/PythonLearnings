ea6edff3ee61c83cf95961c2d1df049b8ada2561
Peter <pi.dudka@gmail.com>
2020-05-06 06:18:30 -0400

    Adding functions.py

--- functions.py
+++ functions.py
@@ -1,5 +1,9 @@
+# All functions start with def(params)
+# Mind your identation!
 def mean(mylist):
     the_mean = sum(mylist) / len(mylist)
     return the_mean
+
+# All functions must have a return statement
 
 print(mean([1,5,12]))
