7003c7bbe812d7b9dee5ffbeda8d1923035a5c7e
Peter <pi.dudka@gmail.com>
2020-05-26 07:22:57 -0400

    Playing around with pandas

--- moreimports.py
+++ moreimports.py
@@ -5,7 +5,7 @@ import pandas
 while True:
     if os.path.exists("Files/original.csv"):
         data = pandas.read_csv("Files/original.csv")
-        print(data.mean()["st1"])
+        print(data.mean()["st2"])
     else:
         print("File does not exist!")
     time.sleep(5)
