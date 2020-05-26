65f785c626aa982fc187d1414b1595a7db3b47e8
Peter <pi.dudka@gmail.com>
2020-05-26 06:39:21 -0400

    Playing around with files

--- fileprocessing.py
+++ fileprocessing.py
@@ -1,5 +1,5 @@
 # File processing begins with opening the file
-myfile = open("original.txt")
+myfile = open("Files/original.txt")
 content = myfile.read()
 
 # You should always get in the habit of closing the file!
@@ -11,9 +11,18 @@ myfile.close()
 print(content)
 
 # You can also open a file using a with block
-with open("original.txt") as newfile:
+with open("Files/original.txt") as newfile:
     new_content = newfile.read()
 
 print(new_content)
 
 # The with block will close the file automatically, which makes things simpler
+
+# We'll now write to a new file
+with open("Files/new.txt", "w") as writefile:
+    writefile.write("Green\nOrange\n")
+    writefile.write("Blue")
+
+print(open("Files/new.txt").read())
+
+# Now we're going to append existing content to a file
