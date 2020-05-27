056f00c588be20c0a95c0c56d0a45ff269e7c3ce
Peter <pi.dudka@gmail.com>
2020-05-27 07:49:56 -0400

    Checking for similar words

--- DictionaryApp/app.py
+++ DictionaryApp/app.py
@@ -1,5 +1,6 @@
 # Import our tools
 import json
+import difflib
 
 # Load our data
 data = json.load(open("data.json"))
@@ -14,7 +15,25 @@ def lookup(word):
     if w in data.keys():
         return data[w]
     else:
-        return "I'm sorry, I don't know what to do with that word."
+        # Check for similar words
+        similars = difflib.get_close_matches(word, data.keys())
+
+        # If we found similar words...
+        if len(similars) > 0:
+            # print(similars)
+            # Loop through them and decide which one to use
+            i = 0
+            while i < len(similars):
+                check_similars = input("You entered {}, but did you mean {}? Please enter 'yes' or 'no': ".format(word, similars[i]))
+                if check_similars.lower() == "yes":
+                    new_word = similars[i]
+                    break
+                else:
+                    i += 1
+                    continue
+            return data[new_word]
+        else:
+            return "I'm sorry, I don't know what to do with that word."
 
 # Figure out which word to look up
 definition = input("Please enter a word: ")
