c4734ee66bfa0b15c2ba44f8ac3eccd06f1c65cb
Peter <pi.dudka@gmail.com>
2020-05-27 06:40:16 -0400

    Adding some functionality to our dictionary

--- DictionaryApp/app.py
+++ DictionaryApp/app.py
@@ -1,10 +1,23 @@
-# Load our data
+# Import our tools
 import json
 
+# Load our data
 data = json.load(open("data.json"))
 
+# Set up a function to process our input
+def lookup(word):
+
+    # Normalize our input
+    w = word.lower()
+
+    # Check if our word exists and respond accordingly
+    if w in data.keys():
+        return data[w]
+    else:
+        return "I'm sorry, I don't know what to do with that word."
+
 # Figure out which word to look up
-word = input("Please enter a word: ")
+definition = input("Please enter a word: ")
 
 # Return our definition
-print(data[word])
+print(lookup(definition))
